f3a92b7733fb532760086e9fd4afcd62623e5c6c
Daniel Rincon <danielrincon999@gmail.com>
2018-08-02 08:38:31 -0500

    authentication with username

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    daniel
                 </div>
 
                 <div class="links">
